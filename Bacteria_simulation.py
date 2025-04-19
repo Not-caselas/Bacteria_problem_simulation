1e3d4ab753f592bf9a159f9e1f5f21db9a8b2c9f
Not-caselas <cosras@outlook.com>
2025-04-19 12:41:24 +0200

    Corrected the real probability

--- Bacteria_simulation.py
+++ Bacteria_simulation.py
@@ -29,6 +29,6 @@ if __name__ == "__main__":
         i += 1
     time_ended = round(time.time() - time_start, 2)
     probability = number_extinct/iterations
-    error = round(abs(((probability - 0.411)/0.411))*100, 2)
+    error = round(abs(((probability - 0.414)/0.414))*100, 2)
     print(f'The probability is: {probability}, the simulation took {time_ended} seconds, and the error is: {error}%')
 
