c2709ddf8648ce860155e615121e7a034ecd1b20
Not-caselas <cosras@outlook.com>
2025-04-22 15:56:45 +0200

    Improved code, error is now negligible. Went from 20 to 15 iterations in the function, bringing down the simulation time from ~24s to 3s, then brought the total number of iterations from 10k to 100k, bringing the time to 30s but the error to ~0%

--- Bacteria_simulation.py
+++ Bacteria_simulation.py
@@ -2,11 +2,10 @@ import random
 import time
 
 def function():
-
     bacteria_number = 1
     i = 0
 
-    while i < 20 and bacteria_number != 0:
+    while i < 15 and bacteria_number != 0:
         for j in range(bacteria_number):
             number = random.randint(0, 3)
             if number == 3:
@@ -16,17 +15,20 @@ def function():
         i += 1
     return bacteria_number
 
-
-if __name__ == "__main__":
+def main():
     time_start = time.time()
     number_extinct = 0
-    iterations = 10000
+    iterations = 100000
     for i in range(iterations):
         extinct = function()
         if extinct == 0:
             number_extinct += 1
     time_ended = round(time.time() - time_start, 2)
-    probability = number_extinct/iterations
+    probability = round(number_extinct/iterations, 3)
     error = round(abs(((probability - 0.414)/0.414))*100, 2)
     print(f'The probability is: {probability}, the simulation took {time_ended} seconds, and the error is: {error}%')
+
+
+if __name__ == "__main__":
+    main()
 
